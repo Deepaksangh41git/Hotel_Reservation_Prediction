2a23bf536e49d89ec153a2b716537594a004dc63
deeps <deepaksangh45@gmail.com>
2026-03-22 00:26:18 +0530

    Data Preprocessing Pipeline Completed

--- utils/common_functions.py
+++ utils/common_functions.py
@@ -3,6 +3,7 @@ import pandas
 from src.logger import get_logger
 from src.custom_exception import CustomException
 import yaml
+import pandas as pd
 
 logger =get_logger(__name__)
 
@@ -18,4 +19,13 @@ def read_yaml(file_path):
     except Exception as e:
         logger.error("Error while reading YAML file")
         raise CustomException("Failed to read the YAML file ",e)
-        
+
+
+def load_data(path):
+    try:
+        logger.info("Loading Data")
+        return pd.read_csv(path)
+    except Exception as e:
+        logger.error(f"Error loading the data {e}")
+        raise CustomException("Failed to load data")
+
